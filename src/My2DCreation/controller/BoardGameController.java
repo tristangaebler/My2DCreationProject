adf7f88e1c55951e584b388779bac98652c6f3d0
tristangaebler <Tristan.Gaebler@gmail.com>
2016-03-01 21:58:32 -0700

    Added to the controller

--- src/My2DCreation/controller/BoardGameController.java
+++ src/My2DCreation/controller/BoardGameController.java
@@ -1,7 +1,20 @@
 package My2DCreation.controller;
 
+import My2DCreation.model.*;
+import My2DCreation.view.*;
+
 public class BoardGameController
 {
+	
+	private BoardGameFrame  baseFrame;
+	private Boardgame [][] myGame;
+	
+	
+	public BoardGameController()
+	{
+		myGame = new Baordgame[4][4];
+		
+	}
 
 	public void start()
 	{
